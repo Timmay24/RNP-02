521716c025e11294fe3b8dd209b5ec8d1400c115
timmay24 <tim.hartig@haw-hamburg.de>
2015-11-13 03:54:01 +0100

    Replace anonymous single functions by lamdas, cleanup stuff, change look n feel

--- src/de/haw/java/client/ChatUI.java
+++ src/de/haw/java/client/ChatUI.java
@@ -1,26 +1,8 @@
 package de.haw.java.client;
 
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-
-import javax.swing.AbstractButton;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class ChatUI {
 	
@@ -41,83 +23,60 @@ public class ChatUI {
 
 
 	public void startUi() {
+
+		// Erst mal die System-Optik aufzwingen
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+
 		this.dialog = new JFrame();
 		contentPane = dialog.getContentPane();
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-		nickButton = new JButton("Nick");
-		nickButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				final JDialog nickChangeDialog = new JDialog();
-				Container nickChangeContainer = nickChangeDialog.getContentPane();
-				nickChangeContainer.setLayout(new BoxLayout(nickChangeContainer, BoxLayout.LINE_AXIS));
-				JLabel label = new JLabel("New Nickname?");
-				nickChangeContainer.add(label);
-				final JTextField inputArea = new JTextField();
-				inputArea.setPreferredSize(new Dimension(100, 16));
-				nickChangeContainer.add(inputArea);
-				JButton okButton = new JButton("OK");
-				okButton.addActionListener(new ActionListener() {
-					
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						String input = inputArea.getText();
-						if (!"".equals(input)) {
-							client.writeToServer("/rename " + input);
-							setName(input);
-							nickChangeDialog.setVisible(false);
-							nickChangeDialog.dispose();
-						}
-					}
-				});
-				nickChangeContainer.add(okButton);
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.addActionListener(new ActionListener() {
-					
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						nickChangeDialog.setVisible(false);
-						nickChangeDialog.dispose();
-					}
-				});
-				nickChangeContainer.add(cancelButton);
-				nickChangeDialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-				nickChangeDialog.setSize(600, 300);
-				nickChangeDialog.setVisible(true);
-			}
-		});
+		nickButton = new JButton("Rename");
+		nickButton.addActionListener(e -> {
+            final JDialog nickChangeDialog = new JDialog();
+            Container nickChangeContainer = nickChangeDialog.getContentPane();
+            nickChangeContainer.setLayout(new BoxLayout(nickChangeContainer, BoxLayout.LINE_AXIS));
+            JLabel label = new JLabel("New Nickname?");
+            nickChangeContainer.add(label);
+            final JTextField inputArea1 = new JTextField();
+            inputArea1.setPreferredSize(new Dimension(100, 16));
+            nickChangeContainer.add(inputArea1);
+            JButton okButton = new JButton("OK");
+            okButton.addActionListener(e1 -> {
+                String input = inputArea1.getText();
+                if (!"".equals(input)) {
+                    client.writeToServer("/rename " + input);
+                    setName(input);
+                    nickChangeDialog.setVisible(false);
+                    nickChangeDialog.dispose();
+                }
+            });
+            nickChangeContainer.add(okButton);
+            JButton cancelButton = new JButton("Cancel");
+            cancelButton.addActionListener(e1 -> {
+                nickChangeDialog.setVisible(false);
+                nickChangeDialog.dispose();
+            });
+            nickChangeContainer.add(cancelButton);
+            nickChangeDialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            nickChangeDialog.setSize(600, 300);
+            nickChangeDialog.setVisible(true);
+        });
 		buttonPanel.add(nickButton);
 		whoButton = new JButton("Who?");
-		whoButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				client.writeToServer("/users");
-			}
-		});
+		whoButton.addActionListener(e -> client.writeToServer("/users"));
 		buttonPanel.add(whoButton);
 		logoffButton = new JButton("Logoff");
-		logoffButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				client.shutDown();
-				dialog.setVisible(false);
-				dialog.dispose();
-			}
-		});
+		logoffButton.addActionListener(e -> shutdown());
 		buttonPanel.add(logoffButton);
 		clearButton = new JButton("Clear");
-		clearButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				chatArea.setText("");
-			}
-		});
+		clearButton.addActionListener(e -> chatArea.setText(""));
 		buttonPanel.add(clearButton);
 		contentPane.add(buttonPanel);
 		chatArea = new JTextArea();
@@ -133,15 +92,13 @@ public class ChatUI {
 			
 			@Override
 			public void keyTyped(KeyEvent e) {
-				// TODO Auto-generated method stub
-				
 			}
 			
 			@Override
 			public void keyReleased(KeyEvent e) {
 				String input = inputArea.getText();
 				if (input.length() > 1 && input.contains("\n")) {
-					System.err.println(input.length());;
+					System.err.println(input.length());
 					inputArea.setText("");
 					client.writeToServer(input);
 					addChatMessageBySelf(input);
@@ -150,53 +107,39 @@ public class ChatUI {
 			
 			@Override
 			public void keyPressed(KeyEvent e) {
-				// TODO Auto-generated method stub
-				
 			}
 		});
 		contentPane.add(inputArea);
 		dialog.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		WindowListener listener = new WindowListener() {
-			
+
 			@Override
 			public void windowOpened(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
 			}
-			
+
 			@Override
 			public void windowIconified(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
 			}
-			
+
 			@Override
 			public void windowDeiconified(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
 			}
-			
+
 			@Override
 			public void windowDeactivated(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
 			}
-			
+
 			@Override
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
-			
+
 			@Override
 			public void windowClosed(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
 			}
-			
+
 			@Override
 			public void windowActivated(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
 			}
 		};
 		dialog.addWindowListener(listener);
@@ -204,9 +147,10 @@ public class ChatUI {
 		dialog.setVisible(true);
 	}
 
-
-	public Component getDialogPane() {
-		return contentPane;
+	private void shutdown() {
+		client.shutDown();
+		dialog.setVisible(false);
+		dialog.dispose();
 	}
 
 
